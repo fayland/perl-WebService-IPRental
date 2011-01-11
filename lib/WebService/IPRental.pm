a8f9f5179f5e328b61b89c136e71853e8355384a
Fayland Lam <fayland@gmail.com>
2011-01-11 15:54:59 +0800

    a working blabla

--- lib/WebService/IPRental.pm
+++ lib/WebService/IPRental.pm
@@ -1,4 +1,4 @@
-package WebService::IPERental;
+package WebService::IPRental;
 
 # ABSTRACT: Todoist API
 
@@ -7,6 +7,7 @@ use warnings;
 use SOAP::Lite;
 #use SOAP::Lite +trace => 'all';
 use Carp 'croak';
+use XML::Simple 'XMLin';
 
 $SOAP::Constants::PREFIX_ENC = 'SOAP-ENC';
 
@@ -16,7 +17,26 @@ $SOAP::Constants::PREFIX_ENC = 'SOAP-ENC';
 
 =head1 DESCRIPTION
 
+    use WebService::IPRental;
 
+    my $ipr = WebService::IPRental->new(
+        APIkey   => $APIkey,
+        APIpass  => $APIpass,
+        Username => $Username,
+        Password => $Password,
+        TTL      => 780, # optional
+        LOcation => 0, # optional
+    );
+    
+    my $resp = $ipr->doIpLease();
+    if ($resp->{Response} == '202' or $resp->{Response} == '203') {
+        print "Res:  " . $ipr->verboseReponseCode($resp->{Response}) . "\n";
+        print "IP:   " . $ipr->verboseReponseCode($resp->{IP}) . "\n";
+        print "Port: " . $ipr->verboseReponseCode($resp->{Port}) . "\n";
+        print "TTL:  " . $ipr->verboseReponseCode($resp->{TTL}) . "\n";
+    } else {
+        die 'Caught error: ' . $ipr->verboseReponseCode($resp->{Response});
+    }
 
 =cut
 
@@ -45,21 +65,58 @@ sub doIpLease {
 
     my @search_request_params;
     foreach my $arg ( qw(APIkey APIpass username password )) {
-        push @search_request_params, SOAP::Data->name( $arg )->value( $self->{ucfirst $arg} );
+        push @search_request_params, SOAP::Data->name( $arg )->value( $self->{ucfirst $arg} )
+            ->attr( { 'xsi:type' => "xsd:string" } );
     }
     foreach my $arg ('TTL', 'Location') {
-        push @search_request_params, SOAP::Data->name( $arg )->value( $self->{$arg} );
+        push @search_request_params, SOAP::Data->name( $arg )->value( $self->{$arg} || 0 )
+            ->attr( { 'xsi:type' => "xsd:int" } );
     }
 
     my $request_params = SOAP::Data->name( 'return' )->value( [@search_request_params] )
       ->attr( { 'xsi:type' => 'ns1:IPRargs' } );
 
     my $xml  = $self->{soap}->call(
-        SOAP::Data->name( 'ns1:doIpLease' )
+        SOAP::Data->name( 'ns1:doIpLease' ),
         $request_params
     );
     
-    return $xml;
+    my $data = XMLin($xml, NoAttr => 1);
+    my $response = $data->{'SOAP-ENV:Body'}->{'ns1:doIpLeaseResponse'}->{return};
+    
+=pod
+    
+            /**
+			 * ========  Response Codes are listed as so ========
+			 * 
+			 * ---- Positive ------------------------------------
+			 *  202 = Good, Fresh IP
+			 *  203 = Good, Duplicate IP
+			 * 
+			 * ---- Negative ------------------------------------
+			 *  402 = Internal Error, Unable to serve IP
+			 *  403 = Unknown User Authentication
+			 *  404 = Unknown API Authentication
+			 *  405 = 0 IP leases left in your pool
+			 *  406 = Impermissible network type
+	 		 */
+
+=cut
+
+    return $response;
+}
+
+sub verboseReponseCode {
+    my ($self, $r) = @_;
+	
+	return "Good, Fresh IP" if $r == 202:
+	return "Good, Duplicate IP" if $r == 203;
+	return "Internal Error, Unable to serve IP" if $r == 402;
+	return "Unknown User Authentication" if $r == 403;
+	return "Unknown API Authentication" if $r == 404;
+    return "0 IP leases left in your pool" if $r == 405;
+	return "Impermissible network type" if $r == 406;
+	return "Unknown";
 }
 
 1;
