033379c5a0b4c16293551ee9ad9ded2b6fb05548
Fayland Lam <fayland@gmail.com>
2011-01-11 15:24:35 +0800

    get more code in

--- WebService/IPRental.pm
+++ WebService/IPRental.pm
@@ -5,7 +5,10 @@ package WebService::IPERental;
 use strict;
 use warnings;
 use SOAP::Lite;
+#use SOAP::Lite +trace => 'all';
 use Carp 'croak';
+
+$SOAP::Constants::PREFIX_ENC = 'SOAP-ENC';
 
 =pod
 
@@ -25,6 +28,14 @@ sub new {
     $args->{APIpass} or croak 'APIpass is required';
     $args->{Username} or croak 'Username is required';
     $args->{Password} or croak 'Password is required';
+    
+    $args->{TTL} ||= 780;
+    $args->{soap} = SOAP::Lite
+      ->envprefix( 'SOAP-ENV' )->on_fault( sub { print( Dumper( $_[1]->fault ), "\n" ); } )
+      ->on_action( sub { 'urn:IPRentalSoapAPIAction' } )
+      ->proxy( 'https://secure.iprental.com/api/',
+        timeout => 5 )->ns( 'urn:IPRentalSoapAPI', 'ns1' )
+      ->outputxml( 1 )->autotype( 0 );
 
     bless $args, $class;
 }
@@ -32,7 +43,23 @@ sub new {
 sub doIpLease {
     my ($self) = @_;
 
+    my @search_request_params;
+    foreach my $arg ( qw(APIkey APIpass username password )) {
+        push @search_request_params, SOAP::Data->name( $arg )->value( $self->{ucfirst $arg} );
+    }
+    foreach my $arg ('TTL', 'Location') {
+        push @search_request_params, SOAP::Data->name( $arg )->value( $self->{$arg} );
+    }
 
+    my $request_params = SOAP::Data->name( 'return' )->value( [@search_request_params] )
+      ->attr( { 'xsi:type' => 'ns1:IPRargs' } );
+
+    my $xml  = $self->{soap}->call(
+        SOAP::Data->name( 'ns1:doIpLease' )
+        $request_params
+    );
+    
+    return $xml;
 }
 
 1;
